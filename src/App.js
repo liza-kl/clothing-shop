fc09f7a1946143e0a20848f545d73307c18d56e1
liza-kl <l.kletsko0202@gmail.com>
2020-11-17 20:53:32 +0100

    :added shoppage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-    <div>
-        <h1>Hatsi hats</h1>
-    </div>);
+import ShopPage from "./pages/shop/shop.component";
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-        <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
-        </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/shop" component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
