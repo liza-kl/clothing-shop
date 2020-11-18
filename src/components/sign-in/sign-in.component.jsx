79cfcf5d4fb6551c8d67545266fc525ce5cfce55
liza-kl <l.kletsko0202@gmail.com>
2020-11-18 21:44:53 +0100

    :alembic: created first sign in component and interconnected stuff

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import FormInput from "../form-input/form-input.component";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -8,7 +9,7 @@ class SignIn extends React.Component {
 
         this.state = {
             email: '',
-            signin: ''
+            password: ''
         }
     }
 
@@ -30,12 +31,9 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type='email' value={this.state.email} onChange={this.handleChange} required/>
-                    <label>E-mail</label>
-                    <input name="password" type='password' value={this.state.password} onChange={this.handleChange}
-                           required/>
-                    <label>Password</label>
-
+                    <FormInput name="email" type='email' value={this.state.email} handleChange={this.handleChange} label='email' required />
+                    <FormInput name="password" type='password' value={this.state.password} handleChange={this.handleChange}
+                               label='password' required />
                     <input type='submit' value='Submit Form'/>
                 </form>
             </div>
