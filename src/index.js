8c83e0b9ccff73931f1d531baba0c6019b1411db
liza-kl <l.kletsko0202@gmail.com>
2020-11-26 08:21:06 +0100

    :boom: set up some redux stuff

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+    <Provider store={store}>
     <React.StrictMode>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
+    </Provider>
+    ,
     document.getElementById('root')
 );
 
