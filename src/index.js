389b6411bc803f995ad37236f95ed5da1ffb8b32
liza-kl <l.kletsko0202@gmail.com>
2020-12-04 22:49:54 +0100

    :package: added persistor to application

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <Provider store={store}>
     <React.StrictMode>
         <BrowserRouter>
+            <PersistGate persistor={persistor}>
             <App/>
+            </PersistGate>
         </BrowserRouter>
     </React.StrictMode>
     </Provider>
