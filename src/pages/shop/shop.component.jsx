03b43a965b08255afa9220e047984c208336a340
liza-kl <l.kletsko0202@gmail.com>
2020-12-21 15:49:48 +0100

    fixed shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+import { Route } from 'react-router-dom';
+
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-const ShopPage = ({collections}) => (
+import CollectionPage from "../category/collection.component";
+const ShopPage = ({match}) => (
 
             <div className='shop-page'>
-               <CollectionsOverview />
+                <Route exact path={`${match.path}` }component={CollectionsOverview}/>
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
             </div>);
 
 export default ShopPage;
